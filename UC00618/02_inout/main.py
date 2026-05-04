b05dd5564ebd679ad0b9387aa3d2215875d2c169
GoncaloaaF <goncaloaaf@gmail.com>
2026-05-04 11:13:55 +0100

    intro

---
+++ UC00618/02_inout/main.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome: ") # -> str
+print(nome)
+
+
+num_txt = input("Digite um numero: ")
+num = int(num_txt) # falha se não num int
+print(num)
+
+num_txt = input("Digite um numero decimal: ")
+num_decimal = float(num_txt) # falha se não num int
+print(num_decimal)
