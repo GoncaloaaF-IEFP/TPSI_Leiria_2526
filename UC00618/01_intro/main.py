a384dc427861434ff0537b4f9872cd4cc98c1b6f
GoncaloaaF <goncaloaaf@gmail.com>
2026-05-04 11:06:54 +0100

    intro

---
+++ UC00618/01_intro/main.py
@@ -0,0 +1,100 @@
+
+# cmt
+"""
+cmt
+multi
+linha
+"""
+
+
+"""
+
+Alg
+char - 1 char
+str - txt
+int - num
+bool - T/F
+
+C 
+float - num decimal
+double - num decimal
+
+C
+
+
+"""
+
+msg = "Hello World 2"
+print(msg)
+
+msg = "nova msg"
+print(msg)
+
+msg = 123 # EVITAR
+print(msg)
+
+print("---------------")
+
+inteiro = 123
+string = "Ola mundo"
+boolean = True
+num_decimal = 123.456
+numDecimal = 123.456
+
+est_natalidade = "123"
+
+print("---------------")
+
+
+n1 = 10
+n2 = 20
+
+soma = n1 + n2
+print(soma)
+
+mult = n1 * n2
+print(mult)
+
+sub = n1 - n2
+print(sub)
+
+
+div = n2 / n1  # / -> float
+print(div)
+
+
+div = n1 / n2
+print(div)
+
+n1 = 10
+n2 = 2
+
+mod = n1 % n2  # resto da div int -> sp int
+print(mod)
+
+
+n1 = 10
+n2 = 3
+
+mod = n1 % n2
+print(mod)
+
+"""
+0 1 2 0 1 2 0 1 2 0 1
+0 1 2 3 4 5 6 7 8 9 10 
+
+"""
+
+n1 = 10
+n2 = 3
+x = n1 // n2
+print(x)
+
+
+# for(int i = 0;i < 10; i++){
+#   for(int j = 0;j < 10; j++){
+# }
+#}
+
+# numDecimal
+# num_decimal
