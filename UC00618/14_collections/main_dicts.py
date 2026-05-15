ebfc3773c63dbb8a6739bc79e3cd865835886ab8
GoncaloaaF <goncaloaaf@gmail.com>
2026-05-15 12:00:59 +0100

    demo collections

--- UC00618/14_collections/main_dicts.py
+++ UC00618/14_collections/main_dicts.py
@@ -16,4 +16,48 @@ print(pessoa)
 
 # print(pessoa["estado"]) # KeyError: 'estado'
 
-print(pessoa.get("estado", "Sem Estado"))
+print(pessoa.get("estado", True))
+
+#                   |
+#                   |
+#                   V
+print(pessoa.pop("idade")) # 38
+print(pessoa)
+
+print(pessoa.popitem()) # ('Estado', True)
+print(pessoa)
+
+
+del pessoa["escola"]
+print(pessoa)
+
+del pessoa["nome"]
+print(pessoa)
+
+
+pessoa = {
+    "nome": "Pedro",
+    "idade": 25,
+    "escola":"IEFP",
+}
+
+# del pessoa
+# print(pessoa)
+
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+
+# iterar
+
+for elm in pessoa: # mostra as keys
+    print(elm)
+
+print("----")
+for elm in pessoa.values(): # mostra as keys
+    print(elm)
+
+print("----")
+for elm in pessoa.items(): # mostra as keys
+    print(elm)
+
